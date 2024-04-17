deba85c3f4949c3019bfc3158041ba70f517e1b2
ChadReacher <asglus7@gmail.com>
2024-04-17 11:15:15 +0200

    Change TSS entry in GDT: change DPL to 0;

--- kernel/src/core/tss.c
+++ kernel/src/core/tss.c
@@ -6,7 +6,7 @@ volatile tss_entry_t kernel_tss;
 
 void tss_init(u32 idx, u32 kss, u32 kesp) {
 	u32 base = (u32)&kernel_tss;
-	gdt_set_entry(idx, base, base + sizeof(tss_entry_t), 0xE9, 0x4);
+	gdt_set_entry(idx, base, base + sizeof(tss_entry_t), 0x89, 0x4);
 	kernel_tss.ss0 = kss;
 	kernel_tss.esp0 = kesp;
 	kernel_tss.cs = 0x0B;
