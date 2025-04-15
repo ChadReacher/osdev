9f87cbaf74ea38ed0857c33bd422c7e8c9b1cb4e
ChadReacher <asglus7@gmail.com>
2025-04-15 23:23:43 +0300

    Introduce fully functional 'truncate' system call
    Previously, the underlying file system ext2 only supported truncate up
    to the 0 size. It means that we could only wipe the file's content out.
    But we want to fully support the 'truncate' system call so the
    implementation has been developed.
    Note: this version only supports the truncate to the less value than the file
    size itself;

--- kernel/src/fs/ext2/ext2_truncate.c
+++ kernel/src/fs/ext2/ext2_truncate.c
@@ -2,53 +2,45 @@
 #include <timer.h>
 #include <bcache.h>
 #include <vfs.h>
-#include <string.h>
+#include <errno.h>
 
-static void trunc_direct(struct vfs_inode *inode) {
-	struct buffer *buf;
-	u32 i;
-	for (i = 0; i < 12; ++i) {
+static void trunc_direct(struct vfs_inode *inode, u32 block) {
+	for (u32 i = block; i < EXT2_DIRECT_BLOCKS; ++i) {
 		if (!inode->u.i_ext2.i_block[i]) {
 			continue;
 		}
-		buf = bread(inode->i_dev, inode->u.i_ext2.i_block[i]);
-		memset(buf->data, 0, 1024);
-		bwrite(buf);
-		ext2_free_block(inode->i_dev, inode->u.i_ext2.i_block[i]);
+                ext2_free_block(inode->i_dev, inode->u.i_ext2.i_block[i]);
 		inode->u.i_ext2.i_block[i] = 0;
 		inode->i_blocks -= 2;
-		brelse(buf);
 	}
 }
 
-static void trunc_indirect(struct vfs_inode *inode) {
+static void trunc_indirect(struct vfs_inode *inode, u32 block) {
 	struct buffer *buf;
-	u32 i, *blocks;
+	u32 *blocks;
 
 	if (!inode->u.i_ext2.i_block[12]) {
 		return;
 	}
 	buf = bread(inode->i_dev, inode->u.i_ext2.i_block[12]);
 	blocks = (u32 *)buf->data;
-	for (i = 0; i < 256; ++i) {
+	for (u32 i = block; i < EXT2_POINTERS_PER_BLOCK; ++i) {
 		if (!blocks[i]) {
 			continue;
 		}
-		buf = bread(inode->i_dev, inode->u.i_ext2.i_block[i]);
-		memset(buf->data, 0, 1024);
-		bwrite(buf);
-		ext2_free_block(inode->i_dev, blocks[i]);
-		blocks[i] = 0;
-		inode->i_blocks -= 2;
+                ext2_free_block(inode->i_dev, blocks[i]);
+                blocks[i] = 0;
+                inode->i_blocks -= 2;
 	}
 	bwrite(buf);
+	brelse(buf);
+
 	ext2_free_block(inode->i_dev, inode->u.i_ext2.i_block[12]);
 	inode->u.i_ext2.i_block[12] = 0;
 	inode->i_blocks -= 2;
-	brelse(buf);
 }
 
-static void trunc_doubly_indirect(struct vfs_inode *inode) {
+static void trunc_doubly_indirect(struct vfs_inode *inode, u32 block) {
 	u32 i, j;
 	struct buffer *buf, *buf2;
 	u32 *indirect, *blocks;
@@ -58,13 +50,13 @@ static void trunc_doubly_indirect(struct vfs_inode *inode) {
 	}
 	buf = bread(inode->i_dev, inode->u.i_ext2.i_block[13]);
 	indirect = (u32 *)buf->data;
-	for (i = 0; i < 256; ++i) {
+	for (i = 0; i < EXT2_POINTERS_PER_BLOCK; ++i) {
 		if (!indirect[i]) {
 			continue;
 		}
 		buf2 = bread(inode->i_dev, indirect[i]);
 		blocks = (u32 *)buf2->data;
-		for (j = 0; j < 256; ++j) {
+		for (j = block; j < EXT2_POINTERS_PER_BLOCK; ++j) {
 			if (!blocks[j]) {
 				continue;
 			}
@@ -73,21 +65,22 @@ static void trunc_doubly_indirect(struct vfs_inode *inode) {
 			inode->i_blocks -= 2;
 		}
 		bwrite(buf2);
+		brelse(buf2);
 
 		ext2_free_block(inode->i_dev, indirect[i]);
 		indirect[i] = 0;
 		inode->i_blocks -= 2;
 
-		brelse(buf2);
 	}
 	bwrite(buf);
+	brelse(buf2);
+
 	ext2_free_block(inode->i_dev, inode->u.i_ext2.i_block[13]);
 	inode->u.i_ext2.i_block[13] = 0;
 	inode->i_blocks -= 2;
-	brelse(buf2);
 }
 
-static void trunc_triply_indirect(struct vfs_inode *inode) {
+static void trunc_triply_indirect(struct vfs_inode *inode, u32 block) {
 	struct buffer *buf, *buf2, *buf3;
 	u32 *dindirect, *indirect, *blocks;
 	u32 i, j, k;
@@ -97,19 +90,19 @@ static void trunc_triply_indirect(struct vfs_inode *inode) {
 	}
 	buf = bread(inode->i_dev, inode->u.i_ext2.i_block[14]);
 	dindirect = (u32 *)buf->data;
-	for (i = 0; i < 256; ++i) {
+	for (i = 0; i < EXT2_POINTERS_PER_BLOCK; ++i) {
 		if (!dindirect[i]) {
 			continue;
 		}
 		buf2 = bread(inode->i_dev, dindirect[i]);
 		indirect = (u32 *)buf2->data;
-		for (j = 0; j < 256; ++j) {
+		for (j = 0; j < EXT2_POINTERS_PER_BLOCK; ++j) {
 			if (!indirect[j]) {
 				continue;
 			}
 			buf3 = bread(inode->i_dev, indirect[j]);
 			blocks = (u32 *)buf3->data;
-			for (k = 0; k < 256; ++k) {
+			for (k = block; k < EXT2_POINTERS_PER_BLOCK; ++k) {
 				if (!blocks[k]) {
 					continue;
 				}
@@ -118,39 +111,71 @@ static void trunc_triply_indirect(struct vfs_inode *inode) {
 				inode->i_blocks -= 2;
 			}
 			bwrite(buf3);
+			brelse(buf3);
+
 			ext2_free_block(inode->i_dev, indirect[j]);
 			indirect[j] = 0;
 			inode->i_blocks -= 2;
-
-			brelse(buf3);
 		}
 		bwrite(buf2);
+		brelse(buf2);
+
 		ext2_free_block(inode->i_dev, dindirect[i]);
 		dindirect[i] = 0;
 		inode->i_blocks -= 2;
-
-		brelse(buf2);
 	}
 	bwrite(buf);
+	brelse(buf);
+
 	ext2_free_block(inode->i_dev, inode->u.i_ext2.i_block[14]);
 	inode->u.i_ext2.i_block[14] = 0;
 	inode->i_blocks -= 2;
-
-	brelse(buf);
 }
 
-/* TODO: For now we assume truncate to size 0
- * refactor the code to reuse the same functionality
- * should we free the contents of the disk block?
- */
-i32 ext2_truncate(struct vfs_inode *inode) {
-	trunc_direct(inode);
-	trunc_indirect(inode);
-	trunc_doubly_indirect(inode);
-	trunc_triply_indirect(inode);
-	inode->i_size = 0;
-	inode->i_dirt = 1;
+// For now, we keep the invariant the given `length` is always less then the
+// actual file size `inode->i_size`
+i32 ext2_truncate(struct vfs_inode *inode, u32 length) {
+        if (length > inode->i_size) {
+                // NB: Current implementation does not support extending a file beyond
+                // its current size.
+                vfs_iput(inode);
+                return -EPERM;
+        }
+
+        u32 block = (length + inode->i_sb->s_block_size - 1) / inode->i_sb->s_block_size;
+
+        if (block < EXT2_DIRECT_BLOCKS) {
+                trunc_direct(inode, block);
+                block = 0;
+        }
+
+        if (block) {
+                block -= EXT2_DIRECT_BLOCKS;
+        }
+        if (block < EXT2_DIRECT_BLOCKS + EXT2_POINTERS_PER_BLOCK * 1) {
+                trunc_indirect(inode, block);
+                block = 0;
+        }
+
+        if (block) {
+                block -= EXT2_POINTERS_PER_BLOCK;
+        }
+        if (block < EXT2_DIRECT_BLOCKS + EXT2_POINTERS_PER_BLOCK * 2) {
+                trunc_doubly_indirect(inode, block);
+                block = 0;
+        }
+
+        if (block) {
+                block -= EXT2_POINTERS_PER_BLOCK;
+        }
+        if (block < EXT2_DIRECT_BLOCKS + EXT2_POINTERS_PER_BLOCK * 3) {
+	        trunc_triply_indirect(inode, block);
+                block = 0;
+        }
+
+	inode->i_size = length;
 	inode->i_mtime = inode->i_ctime = get_current_time();
+	inode->i_dirt = 1;
 	return 0;
 }
 
